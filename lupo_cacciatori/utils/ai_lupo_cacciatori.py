5aba4248b6f7801a8f63d8b966618a85970f9c37
Giacomo Gonzato <75306061+GiacomoGonzato@users.noreply.github.com>
2021-03-10 23:14:09 +0100

    Inizio implementazione ordinare le mosse in ordine probabilmente più conveniente per speed-up

--- lupo_cacciatori/utils/ai_lupo_cacciatori.py
+++ lupo_cacciatori/utils/ai_lupo_cacciatori.py
@@ -1,5 +1,5 @@
 from utils.plancia_lupo_cacciatori import plancia, index_to_coo
-from utils.transposition_table import scelta_casuale, transposition_table
+from utils.transposition_table import transposition_table
 from math import *
 from timeit import default_timer as timer
 
@@ -15,50 +15,6 @@ def check_rotte_possibili(board, simbolo, posto_cacciatore=0):
             if not board.check_movimento_cacciatore(posto_cacciatore, rotta):
                 rotte_possibili.append(rotta)
     return rotte_possibili
-
-
-# True ---> i cacciatori hanno lasciato dei buchi tra di loro
-# False --> non l'hanno fatto
-# Se non sono in formazione penalizzo
-def check_cacciatori_formazione(board):
-    posti_cacciatori = board.find('O')
-    copia_posti = [x for x in posti_cacciatori]
-    copia_posti.sort()
-    righe_cacciatori = {x//8 for x in posti_cacciatori}
-    if len(righe_cacciatori) >= 3:
-        return True
-    elif len(righe_cacciatori) <= 1:
-        return False
-    else:
-        for i in range(3):
-            if (min(posti_cacciatori)//8) % 2 == 0:
-                if ((copia_posti[i+1] - copia_posti[i]) not in {2, 9}):
-                    return True
-            else:
-                if ((copia_posti[i+1] - copia_posti[i]) not in {2, 3}):
-                    return True
-    return False
-
-
-def middle_value(valori):
-    return (sum(valori) / len(valori))
-
-
-def deviazione_standard(valori):
-    media = middle_value(valori)
-    valori_quadri_centrati = [(i - media)**2 for i in valori]
-    return sqrt(middle_value(valori_quadri_centrati))
-
-
-def deviazione_from_index(valori):
-    punti = {index_to_coo(x) for x in valori}
-    return deviazione_from_coo(punti)
-
-
-def deviazione_from_coo(punti):
-    valori_x = [i[0] for i in punti]
-    valori_y = [i[1] for i in punti]
-    return (deviazione_standard(valori_x), deviazione_standard(valori_y))
 
 
 # I cacciatori vogliono minimizzare il valore (mandare il lupo verso l'alto)
@@ -131,25 +87,8 @@ def valore_mossa(tabella, board, contatore, deep, alpha=- inf, beta=inf):
     return valore
 
 
-def conteggio_extra_inizio_lupo(banco):
-    totale = 0
-    (deviazione_x, deviazione_y) = deviazione_from_index(banco.find('O'))
-    totale += deviazione_y
-    return totale
-
-
-def conteggio_extra_inizio_cacciatori(banco):
-    totale = 0
-    if check_cacciatori_formazione(banco):
-        totale += 20
-    (deviazione_x, deviazione_y) = deviazione_from_index(banco.find('O'))
-    totale += deviazione_y
-    return totale
-
-
 # AIUTO SUPPLEMENTARE AL DEBUGGER ----------
 def debug_value(valore, contatore, rotta, coordinata=0):
-
     if True:
         if contatore % 2 == 0:
             print(str(rotta) + ':-------->' + str(valore))
@@ -164,10 +103,15 @@ def scegli_mossa_ai(board, contatore, deep):
     # Devo scegliere la mossa come se fossi il lupo
     # Il lupo vuole massimizzare
     if contatore % 2 == 0:
+        if deep % 2 == 0:
+            deep -= 1
+        if index_to_coo(board.find('X'))[1] > 2 + max({index_to_coo(hunter)[1] for hunter in board.find('O')}):
+            deep = 7
         valore = - inf
         for rotta in check_rotte_possibili(board, 'X'):
             if valore == - inf:
                 mossa_futura = rotta
+            start = timer()
             banco_prova = plancia()
             banco_prova.plancia = [x for x in board.plancia]
             banco_prova.posiziona_lupo(rotta)
@@ -177,34 +121,40 @@ def scegli_mossa_ai(board, contatore, deep):
             if valore < move_power:
                 mossa_futura = rotta
                 valore = move_power
+            end = timer()
+            print(end - start)
+            print()
             if valore == inf:
                 break
             del banco_prova
     # Devo scegliere quale cacciatore muovere e come muoverlo
     # I cacciatori vogliono minimizzare
     else:
+        if deep % 2 == 1:
+            deep -= 1
         valore = inf
+        pool_mosse = list()
         for coordinata in board.find('O'):
-            for rotta in check_rotte_possibili(board, 'O', coordinata):
-                if valore == inf:
-                    mossa_futura = (coordinata, rotta)
-                start = timer()
-                banco_prova = plancia()
-                banco_prova.plancia = [x for x in board.plancia]
-                banco_prova.posiziona_cacciatore(coordinata, rotta)
-                move_power = valore_mossa(tabella,
-                                          banco_prova, contatore + 1, deep - 1, - inf, valore)
-                debug_value(move_power, contatore, rotta, coordinata)
-                if valore > move_power:
-                    mossa_futura = (coordinata, rotta)
-                    valore = move_power
-                if valore == - inf:
-                    break
-                del banco_prova
-                end = timer()
-                print(end - start)
-                print()
+            pool_mosse.extend([(coordinata, rotta)
+                               for rotta in check_rotte_possibili(board, 'O', coordinata)])
+        for mossa in pool_mosse:
+            if valore == inf:
+                mossa_futura = mossa
+            start = timer()
+            banco_prova = plancia()
+            banco_prova.plancia = [x for x in board.plancia]
+            banco_prova.posiziona_cacciatore(mossa[0], mossa[1])
+            move_power = valore_mossa(tabella,
+                                      banco_prova, contatore + 1, deep - 1, - inf, valore)
+            debug_value(move_power, contatore, mossa[1], mossa[0])
+            if valore > move_power:
+                mossa_futura = mossa
+                valore = move_power
+            end = timer()
+            print(end - start)
+            print()
             if valore == - inf:
                 break
+            del banco_prova
     del tabella
     return mossa_futura
